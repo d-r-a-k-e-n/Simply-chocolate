47c8568f872e09d90d0136d7b4e07b080d885162
d-r-a-k-e-n <vnykigor@gmail.com>
2024-10-01 21:47:49 +0300

    added the section how it's made, and an adaptive for it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/header/Header";
 import HeroSection from "./components/heroSection/HeroSection";
 import BenefitSection from "./components/benefitSection/BenefitSection";
 import IngredientSection from "./components/ingredientSection/IngredientSection";
+import MadeSection from "./components/madeSection/MadeSection";
 import TopSellersSection from "./components/topSellersSection/TopSellersSection";
 import SubcribeSection from "./components/subcribeSection/SubcribeSection";
 import Footer from "./components/footer/Footer";
@@ -14,6 +15,7 @@ export default function App() {
         <HeroSection />
         <BenefitSection />
         <IngredientSection />
+        <MadeSection />
         <TopSellersSection />
         <SubcribeSection />
       </main>
--- src/components/madeSection/MadeSection.jsx
+++ src/components/madeSection/MadeSection.jsx
@@ -1,3 +1,62 @@
 import "./madeSection.css";
 
-export default function MadeSection() {}
+export default function MadeSection() {
+  return (
+    <section className="made-section">
+      <div className="container">
+        <h2 className="made-section__title">
+          how it’s <span className="title-accent">made?</span>
+        </h2>
+        <div className="made-section__box">
+          <iframe
+            className="made-section__video"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/JPwZv4hnvYs?si=dUXl-MefRT1kc9CY"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            loading="lazy"
+          ></iframe>
+          <div className="made-section__list-box">
+            <h3 className="made-section__list-title">
+              Try our chocolates today and discover the perfect balance of
+              flavor
+            </h3>
+            <ul className="made-section__list">
+              <li className="made-section__item">
+                <p>
+                  Heat the milk in a saucepan without bringing it to a boil. Add
+                  sugar and cocoa powder.
+                </p>
+              </li>
+              <li className="made-section__item">
+                <p>
+                  At the same time, prepare a water bath. Melt the butter on it.
+                </p>
+              </li>
+              <li className="made-section__item">
+                <p>
+                  "Mix" the milk mixture and already liquid butter. Mix
+                  thoroughly with a silicone spatula so that the mass becomes
+                  uniform and silky.
+                </p>
+              </li>
+              <li className="made-section__item">
+                <p>Heat the mixture over low heat, letting it barely boil.</p>
+              </li>
+              <li className="made-section__item">
+                <p>
+                  Pour the chocolate into special containers (can be replaced
+                  with ice molds). Allow to cool and harden.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
